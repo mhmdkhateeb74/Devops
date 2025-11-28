780d669dd8e4241cd17cb4f2ddeee9158b995fb6
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-11-28 11:03:07 +0200

    deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = 3000;
+const port = 8080;
 
 // مسار ثابت لملفات HTML أو أي ملفات
 app.use(express.static(path.join(__dirname, 'public')));
