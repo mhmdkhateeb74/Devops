27ebd89fcd0b736db0709611f8c8db02bb1e1220
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-12-19 09:37:59 +0200

    last

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const app = express();
 
 const port = 8080;
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
-  res.send('Express server with path is running!');
+  res.sendFile(path.join(__dirname, 'myweb', 'web.html'));
 });
 
-
-   
-
-
-
+app.get('/second', (req, res) => {
+  res.sendFile(path.join(__dirname, 'myweb', 'second.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
